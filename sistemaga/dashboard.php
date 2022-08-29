1b41730fec0effdadf1d9071a75ccf862b6ccbf3
ojosuwu <112409403+ojosuwu@users.noreply.github.com>
2022-08-29 17:23:18 -0600

    Add files via upload

---
+++ sistemaga/dashboard.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sismaga</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/dashboard.css">
+
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row justify-content-center align-content-center">
+            <div class="col-8 barra">
+           
+                <img src="img/maga.png"
+     width="90"
+     height="90">
+            </div>
+            <div class="col-4 text-right barra">
+                <ul class="navbar-nav mr-auto">
+                    <li>
+                        <a href="#" class="px-3 text-light perfil dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="fas fa-user-circle user"></i></a>
+
+                        <div class="dropdown-menu" aria-labelledby="navbar-dropdown">
+                            <a class="dropdown-item menuperfil cerrar" href="#"><i class="fas fa-sign-out-alt m-1"></i>Salir
+                            </a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="barra-lateral col-12 col-sm-auto">
+                <nav class="menu d-flex d-sm-block justify-content-center flex-wrap">
+                    
+                    <a href="#"><i class="fas fa-home"></i><span>Capacitaciones</span></a>
+                    <a href="#"><i class="fas fa-home"></i><span>Nueva Capacitacion</span></a>
+                    <a href="#"><i class="fas fa-home"></i><span>CADER</span></a>
+                    <a href="#"><i class="fas fa-home"></i><span>Nuevo CADER</span></a>
+                    
+                </nav>
+            </div>
+            <main class="main col">
+                <div class="row justify-content-center align-content-center text-center">
+                    <div class="columna col-lg-6">
+                        <h1>Misión</h1> 
+Somos una Institución del Estado, que fomenta el desarrollo rural integral a través de la transformación y modernización del sector agropecuario, forestal e hidrobiológico, desarrollando capacidades productivas, organizativas y comerciales para lograr la seguridad y soberanía alimentaria y competitividad con normas y regulaciones claras para el manejo de productos en el mercado nacional e internacional, garantizando la sostenibilidad de los recursos naturales.
+
+ <h1>Visión</h1>  
+Ser una institución pública eficiente, eficaz y transparente que promueve el desarrollo sustentable y sostenible del sector, para que los productores agropecuarios, forestales e hidrobiológicos, obtengan un desarrollo rural integral a través del uso equitativo de los medios de producción y uso sostenible de los recursos naturales renovables, mejorando su calidad de vida, seguridad y soberanía alimentaria, y competitividad.
+                    </div>
+
+                </div>
+
+            </main>
+        </div>
+    </div>
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://kit.fontawesome.com/646c794df3.js"></script>
+</body>
+
+</html>
